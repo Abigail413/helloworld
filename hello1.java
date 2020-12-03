42b60c0c60dd4d22898d0cd79cd89985e5cf4da4
Abigail413 <56323310+Abigail413@users.noreply.github.com>
2020-12-03 16:11:25 -0600

    Add files via upload

---
+++ hello1.java
@@ -0,0 +1,11 @@
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
+public class Sleep {
+    public static void main (String[] args) {
+        System.out.println(" I need more sleep");
+    }
+}
